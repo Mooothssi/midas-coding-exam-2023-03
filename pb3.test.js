d70c4725d417ba8c5a4f5c10dca731538f50bb98
Tharathorn B <Mooothssi@gmail.com>
2023-03-18 01:00:59 +0700

    Enable all tests for Pb.3

--- pb3.test.js
+++ pb3.test.js
@@ -30,14 +30,14 @@ describe('(2) Problem 3: Code Tests (large numbers)', () => {
     expect(primeAt(50000)).toBe(611953)
   })
 
-  // test('2.2. primeAt(1000000) = 15485863 ✅ (X-Ref from WolframAlpha)', () => {
-  //   expect(primeAt(1000000)).toBe(15485863)
-  // })
-  // /**
-  //  * TODO: Should use Prime Number Theorem / Chebyshev's Theorem if exceeding this line
-  //  * lim x to +inf (π(x) / [x / ln(x)]) = 1
-  //  */
-  // test('2.3. primeAt(20000000) = 373587883 ❌ (X-Ref from WolframAlpha, failed)', () => {
-  //   expect(primeAt(20000000)).toBe(373587883)
-  // })
+  test('2.2. primeAt(1000000) = 15485863 ✅ (X-Ref from WolframAlpha)', () => {
+    expect(primeAt(1000000)).toBe(15485863)
+  })
+  /**
+   * TODO: Should use Prime Number Theorem / Chebyshev's Theorem if exceeding this line
+   * lim x to +inf (π(x) / [x / ln(x)]) = 1
+   */
+  test('2.3. primeAt(20000000) = 373587883 ❌ (X-Ref from WolframAlpha, failed)', () => {
+    expect(primeAt(20000000)).toBe(373587883)
+  })
 })
