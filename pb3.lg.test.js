4de14488366ac9976fd509858326a266722fca37
Tharathorn B <Mooothssi@gmail.com>
2023-03-18 09:40:20 +0700

    Add more `lg` test cases for Pb.3

--- pb3.lg.test.js
+++ pb3.lg.test.js
@@ -8,7 +8,13 @@
 const { primeAt } = require('./pb3')
 
 describe('(3L) Problem 3: Code Tests (large numbers)', () => {
-  test('3L.1. primeAt(50000) = 611953 ✅ (📖 x-ref: WolframAlpha)', () => {
+  test('3L.1. primeAt(10000) = 104729 ✅ (📖 x-ref: WolframAlpha)', () => {
+    expect(primeAt(10000)).toBe(104729)
+  })
+  test('3L.2. primeAt(50000) = 611953 ✅ (📖 x-ref: WolframAlpha)', () => {
     expect(primeAt(50000)).toBe(611953)
+  })
+  test('3L.3. primeAt(100000) = 1299709 ✅ (📖 x-ref: WolframAlpha)', () => {
+    expect(primeAt(100000)).toBe(1299709)
   })
 })
